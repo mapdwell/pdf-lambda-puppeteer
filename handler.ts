14f80d756900baab1de4b9486e96a27d1dd4a7ae
Nicolas Waissbluth <nwaissbluth@gmail.com>
2020-10-26 18:52:16 -0400

    feat: parse body if not json

--- handler.ts
+++ handler.ts
@@ -2,6 +2,10 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 import { PDFGenerator } from "./src/PDFGenerator";
 
 export const pdf: APIGatewayProxyHandler = async (event, _context) => {
-  const response = await PDFGenerator.getPDF(event);
+  let body = event.body;
+  if (typeof body === 'string') {
+    body = JSON.parse(body);
+  }
+  const response = await PDFGenerator.getPDF(body);
   return response;
 };
